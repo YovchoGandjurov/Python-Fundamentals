531f41f0173b9d2acd0feaf70575c18549d4e686
YovchoGandjurov <yovchoo@gmail.com>
2019-03-21 23:51:34 +0200

    Added task - Sign of Integer Number

---
+++ 01. Functions. Debugging and Troubleshooting code/02.Sign_of_Integer_Number.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+def string(n):
+    if n > 0:
+        print('The number', n, 'is positive.')
+    if n < 0:
+        print('The number', n, 'is negative.')
+    if n == 0:
+        print('The number', n, 'is zero.')
+
+string(n)
