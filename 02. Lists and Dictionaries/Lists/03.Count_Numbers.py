2b1b19ae26f7bb455effe28042716e9abb367f2d
YovchoGandjurov <yovchoo@gmail.com>
2019-03-22 22:38:06 +0200

    removing

--- 02. Lists and Dictionaries/Lists/03.Count_Numbers.py
+++
@@ -1,9 +0,0 @@
-n = [int(x) for x in input().split(' ')]
-unique = set(n)
-
-
-for x in sorted(unique):
-    if x not in range(0, 1000):
-        continue
-    else:
-        print('{} -> {}'.format(x, n.count(x)))
--- 02. Lists and Dictionaries/Lists/06.Reverse_List_In_place.py
+++
@@ -1,8 +0,0 @@
-n = [int(x) for x in input().split(' ')]
-count = -1
-
-for x in range(len(n) // 2):
-    n[x], n[count] = n[count], n[x]
-    count -= 1
-
-print(' '.join([str(x) for x in n]))
